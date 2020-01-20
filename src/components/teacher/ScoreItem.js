bbafcf9f992157cf77063a98f6f00da42b4ba2af
Lauri Kemppainen <laukempp@gmail.com>
2020-01-20 08:01:04 +0200

    Scores-komponentti tehty, questions-array rakennetta muokattu, answereiden mäppäyksessä probleemia

---
+++ src/components/teacher/ScoreItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ScoreItem = ({ question, answers }) => {
+  console.log(question);
+  console.log(answers);
+
+  return <div></div>;
+};
+
+export default ScoreItem;
