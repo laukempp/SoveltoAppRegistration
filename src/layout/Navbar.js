6486acedfa0976a5025e384f6f0f1a62819c64fa
sormunen <sormunen.roni@gmail.com>
2020-01-23 11:36:16 +0200

    mobiilisivusto quizformista ja questionformista tehty, navbar responsiivinen myös

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -4,21 +4,32 @@ import { Link } from "react-router-dom";
 export const Navigation = props => {
   return (
 
-    <Navbar collapseOnSelect expand="sm" className="navbar bg-light">
+  <Navbar collapseOnSelect expand="md" className="navbar bg-light">
       <Nav.Link eventKey={1} as={Link} to="/dashboard">
         <Nav.Item><h3 id="sovelto-red">{props.title}</h3></Nav.Item>
       </Nav.Link>
-      <Nav.Item>
+      
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav className="mr-auto">
         <Nav.Link eventKey={2} as={Link} to="/dashboard/question">
-          Luo kysymyksiä
+          <span className="NavLink">Luo kysymyksiä</span>
         </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey={3} as={Link} to="/logout">
-          Kirjaudu ulos
+      
+      
+        <Nav.Link eventKey={3} as={Link} to="/scores">
+          <span className="NavLink">Tulokset</span>
         </Nav.Link>
-      </Nav.Item>
-    </Navbar>
+      
+        <Nav.Link className="justify-content-end" eventKey={4} as={Link} to="/logout">
+          <span className="NavLink logout">Kirjaudu ulos</span>
+        </Nav.Link>
+     
+      </Nav></Navbar.Collapse>
+      </Navbar> 
+     
+  
+    
   );
 };
 
