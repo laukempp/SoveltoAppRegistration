77aa8ec28b810483a7e3e972f5bfb1dab6bb896a
sormunen <sormunen.roni@gmail.com>
2020-02-12 17:42:45 +0200

    websocket now updates scores component when student finishes quiz and fixed bug where 100% made result display in 2 lines

--- src/components/teacher/Scores.js
+++ src/components/teacher/Scores.js
@@ -2,30 +2,59 @@ import React, { useEffect, useState } from "react";
 import { getScores } from ".//../../service/Request";
 import ScoreItem from "./ScoreItem";
 import { Navigation } from "../../layout/Navbar";
+import socketIOClient from "socket.io-client";
+
 
 const Scores = () => {
   const [scoreData, setScoreData] = useState([]);
+  const [reRender,setreRender] = useState(false);
   // const [questionData, setQuestiondata] = useState([]);
 
   // const fetchScores = () => {
   //   getScores().then(res => setScore(res));
   // };
 
+  const socket = socketIOClient("http://localhost:5001");
+  
   const id = sessionStorage.getItem("badge");
   const badge = { teacher_badge: parseInt(id) };
 
-  console.log(badge);
-
   useEffect(() => {
+      getScores(badge).then(res => {
+      setScoreData(res);
+    });
+  },[]);
+ 
+
+  
+  /* const customEventHandler = () => {
     getScores(badge).then(res => {
       setScoreData(res);
     });
-  }, []);
-  console.log(scoreData);
-
+  } */
+  socket.on("renderScore", ev => {
+    setreRender(true)
+    
+  },10);
+  const timerSet = () => {setTimeout(() => {
+    getScores(badge).then(res => {
+    setScoreData(res);
+  })}, 500)}
+  const timerClear = () => { clearTimeout(timerSet(), 3000)}
+ useEffect(() => {
+  timerSet()
+  timerClear()
+  setreRender(false)
+ },[reRender])
+    
+ 
+  
+  let keyCount = 0;
   const scores = scoreData.map(result => {
+    keyCount++;
     return (
       <ScoreItem
+        key={keyCount}
         result={result}
         id={result.id}
         question={result.question}
@@ -37,7 +66,7 @@ const Scores = () => {
   let howManyAnswers = 0;
   const answers = scoreData.map(result => {
     const answerss = result.results;
-    console.log(answerss);
+    /* console.log(answerss); */
     answerss.map(answerr => {
       howManyAnswers += answerr.count;
       if (answerr.isCorrect === true) {
@@ -45,23 +74,43 @@ const Scores = () => {
       }
     });
   });
-  console.log(answers);
-  console.log(howManyCorrect);
-
-  return (
-    <div className="text-white">
-      <Navigation title={"Soveltommi"} />
-      <br />
-      {/* <h3>Quiz id: {id.quiz_id}</h3> */}
-      <h4>
-        Quiz Total Score:{" "}
-        {Math.round((howManyCorrect / howManyAnswers) * 100 * 100) / 100}%
-      </h4>
-      <div></div>
+ /*  console.log(answers);
+  console.log(howManyCorrect); */
+    let totalCorrect = Math.round((howManyCorrect / howManyAnswers) * 100 * 100) / 100;
+  
+    if(reRender){
+    return (
+      <div className="text-white">
+        <Navigation title={"Soveltommi"} />
+     <br />
+     
+     <h4>
+       Quiz Total Score:{" "}
+       {totalCorrect ? totalCorrect : 0}%
+     </h4>
+     <div></div>
       {scores}
-    </div>
-  );
-
+      </div>
+       );
+}
+    else
+       return (
+        <div className="text-white">
+          <Navigation title={"Soveltommi"} />
+       <br />
+       
+       <h4>
+         Quiz Total Score:{" "}
+         {totalCorrect ? totalCorrect : 0}%
+       </h4>
+       <div></div>
+       {scores}
+        </div>
+         );
+    
+  
+ 
+  
   // console.log(data);
 
   // const modifiedQuestionArray = questionData.map(question => {
