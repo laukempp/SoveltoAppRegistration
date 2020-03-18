86929cb806cf255411d75d0141bc905bd1c41711
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2020-03-18 09:23:46 +0200

    Code cleaning & making sure that questions can be sent from both questiontab and questionpreview

--- src/components/teacher/QuizPreview.js
+++ src/components/teacher/QuizPreview.js
@@ -6,14 +6,22 @@ import { postQuiz } from "../../service/Request";
 import { uuid } from "uuidv4";
 
 export default function QuizPreview({ formProps }) {
+  const {
+    questions,
+    tags,
+    quizSettings,
+    handleClose,
+    showSuccessMessage
+  } = formProps;
 
-  const {questions, tags, quizSettings, handleClose, showSuccessMessage} = formProps;
+  //Muodostetaan valittuja kysymyksiä varten alkutila, jossa otetaan serveriltä tuotujen kysymysten id:t ja asetetaan ne avaimiksi -> jokainen olio on alkuun muotoa {1:false}, jossa 1 on se id-numero, joka on vastaavalla kysymyksellä eli se voi olla mikä vaan numero.
+  const [checkedArray, setCheckedArray] = useState({
+    checkboxes: questions.reduce(
+      (options, option) => ({ ...options, [option.id]: false }),
+      {}
+    )
+  });
 
-  const [checkedArray, setCheckedArray] = useState({checkboxes: questions.reduce(
-    (options, option) => ({ ...options, [option.id]: false }), {}
-  )}) 
-
-  console.log(quizSettings)
   const socket = socketIOClient("http://localhost:5001");
 
   //Funktio, joka kerää opettajan valitsemat kysymykset. Klikkaamalla checkboxia avaimen arvo muuttuu välillä true/false
@@ -39,13 +47,13 @@ export default function QuizPreview({ formProps }) {
     });
   };
 
-  //Funktio, joka rakentaa valituista checkboxeista arrayn, jossa on pelkästään valittujen kysymysten id:t (eli ne, joiden arvo on true)
-  const idArray =
-    checkedArray &&
-    Object.keys(checkedArray.checkboxes)
-      .filter(checkbox => checkedArray.checkboxes[checkbox])
-      .map(checkbox => checkbox);
 
+  //Funktio, joka rakentaa valituista checkboxeista arrayn, jossa on pelkästään valittujen kysymysten id:t (eli ne, joiden arvo on true)
+  const idArray = Object.keys(checkedArray.checkboxes)
+    .filter(checkbox => checkedArray.checkboxes[checkbox])
+    .map(checkbox => checkbox);
+
+  //Käsitellään lomakkeen lähetys - tarkistetaan ensin, onko kysymyksiä valittu, jos on, muodostetaan datasetti ja lähetetään se tietokantaan, sitten suljetaan modaali
   const handleQuizSubmit = e => {
     e.preventDefault();
 
@@ -54,24 +62,25 @@ export default function QuizPreview({ formProps }) {
         "Tyhjää tenttiä ei voi lähettää, ole hyvä ja valitse kysymyksiä lähetettäväksi"
       );
     } else {
-
-    let data = {
-      title: quizSettings.title,
-      question_ids: idArray,
-      quiz_author: sessionStorage.getItem("badge"),
-      quiz_badge: uuid(),
-      istemporary: 0,
-      timer: quizSettings.timer,
-      quiz_type: quizSettings.quiz_type
-    };
-    postQuiz(data)
-  .then(res => {
+      let data = {
+        title: quizSettings.title,
+        question_ids: idArray,
+        quiz_author: sessionStorage.getItem("badge"),
+        quiz_badge: uuid(),
+        istemporary: 0,
+        timer: quizSettings.timer,
+        quiz_type: quizSettings.quiz_type
+      };
+      postQuiz(data)
+        .then(res => {
           showSuccessMessage(res.success);
         })
-      .then(() => socket.emit("eventMessage", data))
-      .then(() => handleClose())
-  }};
+        .then(() => socket.emit("eventMessage", data))
+        .then(() => handleClose());
+    }
+  };
 
+  //Tämän funktion tehtävä on käydä läpi jokaisen kysymyksen q_tags -array ja filtteröidä siitä pois ne tagit, joita käyttäjä ei käyttänyt kyselyssä (koska kysymyksellä voi olla hyvin monta tagia ja käyttäjä esim. käytti vain 2 hakiessaan kysymyksiä)
   const tagFilter = (array1, array2) => {
     array1.forEach(element => {
       if (element.q_tags) {
@@ -83,6 +92,7 @@ export default function QuizPreview({ formProps }) {
     return array1;
   };
 
+  //Tämän funktion tehtävä on järjestää kysymykset uudestaan niin, että ylimpänä ovat ne kysymykset, joilla on eniten käyttäjähakua vastaavia tageja ja viimeisenä ne, joilla on vain yksi
   const sortedQuestions = tagFilter(questions, tags).sort((array1, array2) => {
     if (array2[0]) {
       return array2.q_tags.length - array1.q_tags.length;
@@ -90,6 +100,7 @@ export default function QuizPreview({ formProps }) {
     return "";
   });
 
+  //Muodostetaan checkbox-inputit käymällä läpi kaikki kysymykset
   let checkBoxInput = sortedQuestions.map(option => {
     let count = 0;
     return (
@@ -106,28 +117,48 @@ export default function QuizPreview({ formProps }) {
   return (
     <div>
       <form onSubmit={handleQuizSubmit}>
-
-      {questions[0] ? (<div>
-        <FormButton buttonProps={{buttonText: "Valitse kaikki", handleClick: e => {selectAll(true)}, buttonClass: "chooseAll"}}/>
-        <FormButton buttonProps={{buttonText: "Poista valinnat", handleClick: e => {selectAll(false)}, buttonClass: "removeAll"}}/>
-      </div>) : null}
-      {checkBoxInput}
-      <div className="input-row">
-        <FormButton 
-        buttonProps={{
-          buttonType: "submit",
-          buttonClass: "btnLogin",
-          buttonText: "Lähetä tentti",
-        }} />
-      </div>
-      <br/>
-      <div className="input-row">
-        <FormButton buttonProps={{
-          buttonClass: "btnLogin",
-          buttonText: "Sulje ikkuna",
-          handleClick: handleClose
-        }} />
-      </div>
+        {questions[0] ? (
+          <div>
+            <FormButton
+              buttonProps={{
+                buttonText: "Valitse kaikki",
+                handleClick: e => {
+                  selectAll(true);
+                },
+                buttonClass: "chooseAll"
+              }}
+            />
+            <FormButton
+              buttonProps={{
+                buttonText: "Poista valinnat",
+                handleClick: e => {
+                  selectAll(false);
+                },
+                buttonClass: "removeAll"
+              }}
+            />
+          </div>
+        ) : null}
+        {checkBoxInput}
+        <div className="input-row">
+          <FormButton
+            buttonProps={{
+              buttonType: "submit",
+              buttonClass: "btnLogin",
+              buttonText: "Lähetä tentti"
+            }}
+          />
+        </div>
+        <br />
+        <div className="input-row">
+          <FormButton
+            buttonProps={{
+              buttonClass: "btnLogin",
+              buttonText: "Sulje ikkuna",
+              handleClick: handleClose
+            }}
+          />
+        </div>
       </form>
     </div>
   );
--- src/components/teacher/QuizTab.js
+++ src/components/teacher/QuizTab.js
@@ -10,16 +10,19 @@ import useToggle from "../hooks/useToggle";
 import Modal from "react-bootstrap/Modal";
 import { uuid } from "uuidv4";
 
+//Regex queryn ja tagilistan vertaamista varten, jotta funktio osaa katsoa, mitkä tagit muistuttavat querya
+function escapeForRegExp(query) {
+  return query.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+
 export default function QuizTab({ showSuccessMessage }) {
   const [questions, setQuestions] = useState([]);
   const [topics, setTopics] = useState([]);
-  const [quizSettings, setQuizSettings] = useState({title: '', timer: 0, quiz_type: false});
-  const [suggestions, setSuggestions] = useState();
+  const [quizSettings, setQuizSettings] = useState({title: "", timer: 0, quiz_type: false});
+  const [suggestions, setSuggestions] = useState([]);
   const [tags, setTags] = useState([]);
   const [message, setMessage] = useState("");
-  const [selectedOption, setSelectedOption] = useState();
-
-  console.log(selectedOption)
+  const [selectedOption, setSelectedOption] = useState(null);
 
   //Tuodaan toggle-hook komponentin käyttöön
   const { show, toggleShow, content, showQuiz, showQuestion } = useToggle();
@@ -30,13 +33,13 @@ export default function QuizTab({ showSuccessMessage }) {
     getTags().then(res => setSuggestions(res));
   }, []);
 
+  //Funktio, joka asettaa topicin valinnan - käyttää erillistä react-select -kirjastoa & funktio valutetaan alastapäin formin käyttöön
   const handleTopicAdd = selectedOption => {
-    setSelectedOption(selectedOption)
-    console.log(`Option selected:`, selectedOption)
+    setSelectedOption(selectedOption);
   };
 
   //Muodostetaan tietokantaan lähetettävä tag-Array (array koostuu pelkistä stringeistä)
-  const tagArray = Object.values(tags && tags.map(item => item.name));
+  const tagArray = tags[0] && Object.values(tags.map(item => item.name));
 
   //Tagien poistofunktio
   const handleDelete = i => {
@@ -49,20 +52,28 @@ export default function QuizTab({ showSuccessMessage }) {
     setTags(tags => [...tags, tag]);
   };
 
-  //Funktio, joka sulkee modaali-ikkunan ja samalla resetoi komponentin tilan
-  const handleClose = () => {
-
-    setQuizSettings({title: '', timer: 0, quiz_type: false})
-    setMessage("")
-    setSelectedOption()
-    setTags([])
-    toggleShow();
+  //Suodattaa tagilistalta ne ehdotukset, jotka näytetään. Ensin katsoo, löytyykö valittujen tagien listalta query ja sitten katsoo, sopiiko query regexiin. Tarkoitus on, ettei käyttäjä voi lisätä samaa tagia useampaa kertaa valittujen listaan
+  const checkTagDouble = (suggestion, query) => {
+    const regex = new RegExp(`(?:^|\\s)${escapeForRegExp(query)}`, "i");
+    if (tags.includes(suggestion)) {
+      return false;
+    } else if (regex.test(suggestion.name)) {
+      return true;
+    }
+    return false;
   };
 
-  //Mapataan auki aiheet Formikia varten
-  let options = topics && topics[0] && topics.map(option => {
-    return {value: option.id, label:option.title}
-  });
+  //Funktio, joka sulkee modaali-ikkunan ja samalla resetoi komponentin tilan
+  const handleClose = () => {
+    setQuizSettings({ title: "", timer: 0, quiz_type: false });
+    setMessage("");
+    setTags([]);
+    toggleShow();
+    setSelectedOption(null);
+  };
+
+  //Mapataan auki aiheet Formikia varten ja muodostetaan niistä Select-componentille sopivanmuotoinen olio-array
+  let options = topics[0] && topics.map(option => {return {value: option.id, label: option.title};});
 
   //Kerätään yhteen kaikki propsit, jotka komponentin täytyy välittää lapsille.
   const formProps = {
@@ -77,8 +88,9 @@ export default function QuizTab({ showSuccessMessage }) {
     options: options,
     selectedOption: selectedOption,
     handleTopicAdd: handleTopicAdd,
-    showSuccessMessage
-  }
+    showSuccessMessage: showSuccessMessage,
+    checkTagDouble: checkTagDouble
+  };
 
   return (
     <>
@@ -92,18 +104,20 @@ export default function QuizTab({ showSuccessMessage }) {
             validationSchema={quizValidationSchema}
             onSubmit={(values, { setSubmitting, resetForm }) => {
               setSubmitting(true);
-              console.log(values)
-              fetchQuestions(values)
-                .then(res => {
+              fetchQuestions(values).then(res => {
                   if (res.message) {
-                    setQuestions([]);
                     setMessage(res.message);
                   } else {
                     setQuestions(res);
                   }
                 })
-                .then(() => setTags([]))
-                .then(() => setQuizSettings({title: values.name, timer: values.timer, quiz_type: values.quiz_type}))
+                .then(() =>
+                  setQuizSettings({
+                    title: values.name,
+                    timer: values.timer,
+                    quiz_type: values.quiz_type
+                  })
+                )
                 .then(() => showQuiz())
                 .then(() => toggleShow());
               resetForm();
@@ -120,16 +134,17 @@ export default function QuizTab({ showSuccessMessage }) {
             )}
           </Formik>
 
-
           <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>
-                  {content ? ("Esikatselu Quizille " + quizSettings.title) : ("Luo uusi kysymys ja tentti")}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <div>
-                  {message.length > 1 && (
-                    <div>{message}</div>)}
+            <Modal.Header closeButton>
+              <Modal.Title>
+                {content
+                  ? "Esikatselu Quizille " + quizSettings.title
+                  : "Luo uusi kysymys ja tentti"}
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>
+                {message.length > 1 && <div>{message}</div>}
                 <div className="quizPreview">
                   {content ? (
                     <QuizPreview formProps={formProps} />
